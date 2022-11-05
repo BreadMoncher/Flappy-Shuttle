08f4b1d1c11a49101f8cf3e21c19d93e353dba52
BreadMoncher <aidan0143@gmail.com>
2022-11-05 00:14:20 -0500

    added window

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("hello world!")
+import sys
+ 
+import pygame
+ 
+# initialize pygame
+pygame.init()
+ 
+fps = 60
+fpsClock = pygame.time.Clock()
+ 
+width, height = 640, 480
+screen = pygame.display.set_mode((width, height))
+ 
+# Game loop.
+running = True
+while running:
+    screen.fill((0, 0, 0))
+  
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+  
+    # Update.
+  
+    # Draw.
+  
+    pygame.display.flip()
+    fpsClock.tick(fps)
+
+pygame.quit()
